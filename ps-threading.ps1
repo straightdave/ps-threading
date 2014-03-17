77ffae01c7fdcd6d621368fcaed9d28ad745e102
Dave <eyaswoo@gmail.com>
2014-03-17 22:53:16 +0800

    add scaffold
    ps-threading.ps1 is a scaffold to help you understand how to implement
    multi-threads work async

---
+++ ps-threading.ps1
@@ -0,0 +1,33 @@
+##
+## ps-threading scaffold
+## REAL threading in PowerShell
+## Dave Wu, eyaswoo@gmail.com, Mar 2014
+##
+
+#=======================
+# parameters
+$min = 1  # min runspace(s) in the pool
+$max = 5  # max runspace(s) in the pool
+
+$block = {
+  param($p)
+  
+  # what you want to do in threads
+  $p >> .\log.txt
+}
+
+
+$pool = [RunspaceFactory]::CreateRunspacePool($min,$max)
+$pool.open()
+
+(1..10) | %{
+  $t = [powershell]::create()
+  $t.RunspacePool = $pool
+  [void]$t.AddScript($block)
+  [void]$t.AddParameter("p",$_)
+  
+  $job = $p.BeginInvoke()
+
+  # you can call endinvoke in/out this loop
+  # $p.EndInvoke($job)
+}
